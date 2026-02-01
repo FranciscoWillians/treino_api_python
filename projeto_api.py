0d64661f7c02f0e710ce6735b8e4c7e317f5fb64
FRANCISCO WILLIANS DA VERA CRUZ SILVA <96497148+FranciscoWillians@users.noreply.github.com>
2026-02-01 14:13:29 -0300

    Refatorar os parâmetros da API e os comentários em projeto_api.py

--- projeto_api.py
+++ projeto_api.py
@@ -8,28 +8,28 @@ load_dotenv() #carregas as variáveis de ambiente que criei
 link_api="http://api.weatherapi.com/v1/current.json"
 link_api2="http://api.weatherapi.com/v1/forecast.json"
 
-chave_api = os.getenv('API_KEY')
+chave_api = os.getenv('API_KEY') #chama meu token de forma segura quando subir ao github e não vaar minha credencial
 
-dias_previsao = int(input("digite quantos dias de previsão você quer de 1 a 5:"))
+dias_previsao = int(input("digite quantos dias de previsão você quer de 1 a 5:")) #recebe quantos dias de previsão vai querer receber
 
-parameters ={
-    "key": chave_api,
-    "q": "Teresina",
-    "lang": "pt"
+parameters ={  #parametros que vou enviar a api dizendo o que eu quero, nesse caso eu disse: "ta aqui minha chave, minha cidade é X, quero em portugues"
+    "key": chave_api, #meu token, se eu não enviar volta 403/401
+    "q": "coloque_sua_cidade", #cidade que quero receber os dados poderia colocar sao paulo, rio de janeiro. eu escolho. 
+    "lang": "pt" #esses dados me voltam em portugues
 }
 
-parameters2 ={
-    "key": chave_api,
-    "q": "Teresina",
-    "lang": "pt",
-    "days": dias_previsao
+parameters2 ={ #parametros que vou enviar a api dizendo o que eu quero, nesse caso eu disse: "ta aqui minha chave, minha cidade é X, quero em portugues e quero a previsão."
+    "key": chave_api, #meu token, se eu não enviar volta 403/401
+    "q": "coloque_sua_cidade", #cidade que quero receber os dados poderia colocar sao paulo, rio de janeiro. eu escolho.
+    "lang": "pt", #esses dados me voltam em portugues
+    "days": dias_previsao #coloco quantos dias quero de previsão, essea qtd de dias vem da variável criada acima (dias_previsao) que recebe os dados digitado pelo usuário
 }
 
-resultado_api = requests.get(link_api,params=parameters)
+resultado_api = requests.get(link_api,params=parameters) # traz o resultado do que eu solicitei
 
-if resultado_api.status_code==200:
-    dados_requisicao=resultado_api.json()
-    temperatura = dados_requisicao['current']['temp_c']
+if resultado_api.status_code==200: #se o resutado for 200, que signica "ok" eu transformo a variável "resultado_api" em json na variável a baixo
+    dados_requisicao=resultado_api.json() #transformo o resuktado do que eu pedi em json
+    temperatura = dados_requisicao['current']['temp_c'] #vou trazer 
     descricao = dados_requisicao['current']['condition']['text']
     cidade = dados_requisicao['location']['name']
     estado = dados_requisicao['location']['region']
